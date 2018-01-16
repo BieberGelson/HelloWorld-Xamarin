842230f4c3598f0b36c462aaa99066aa49b22c24
Gelson <Gelson@DESKTOP-UPENJHS>
2018-01-16 10:41:13 -0200

    Adicionar .gitignore e .gitattributes.

---
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetPage : ContentPage
+	{
+        public GreetPage()
+        {
+            InitializeComponent();
+            slider.Value = 0.5;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/StackPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StackPage : ContentPage
+	{
+		public StackPage ()
+		{
+			InitializeComponent ();
+            var layout = new StackLayout
+            {
+                Spacing = 40,
+                Padding = new Thickness(0, 40, 0, 0),
+                Orientation = StackOrientation.Horizontal,
+                BackgroundColor = Color.LightBlue
+            };
+            layout.Children.Add(new Label { Text = "Label 1" });
+            var layout1 = new StackLayout
+            {
+                Padding = 40
+            };
+            layout1.Children.Add(new Label { Text = "Label 2" });
+            layout1.Children.Add(new Label { Text = "Label 3" });
+            layout.Children.Add(layout1);
+            Content = layout;
+		}
+	}
+}
